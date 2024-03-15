3ea2d20e202b2c440b7a2ecf28502c29a834182e
Rodrigo <guigoazzolini@gmail.com>
2024-03-15 15:41:35 -0300

    updated setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,16 +7,11 @@ vim.keymap.set("i", "jk", "<Esc>", {})
 vim.keymap.set("i", "kj", "<Esc>", {})
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
 local opts = { noremap = true, silent = true }
-
--- Move to previous/next
 vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
 vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
--- Re-order to previous/next
 vim.keymap.set("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
 vim.keymap.set("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
--- Goto buffer in position...
 vim.keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
 vim.keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
 vim.keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
@@ -27,26 +22,14 @@ vim.keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
 vim.keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
 vim.keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
 vim.keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
--- Pin/unpin buffer
 vim.keymap.set("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
--- Close buffer
 vim.keymap.set("n", "<A-c>", "<Cmd>BufferClose<CR>", opts)
--- Wipeout buffer
---                 :BufferWipeout
--- Close commands
---                 :BufferCloseAllButCurrent
---                 :BufferCloseAllButPinned
---                 :BufferCloseAllButCurrentOrPinned
---                 :BufferCloseBuffersLeft
---                 :BufferCloseBuffersRight
--- Magic buffer-picking mode
 vim.keymap.set("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
--- Sort automatically by...
 vim.keymap.set("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
 vim.keymap.set("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
 vim.keymap.set("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
 vim.keymap.set("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
 
--- Other:
--- :BarbarEnable - enables barbar (enabled by default)
--- :BarbarDisable - very bad command, should never be used
+vim.keymap.set("n", "<leader>n", function()
+	require("notion").openMenu()
+end)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt_global.spelllang = { "en", "pt-br" }
+vim.filetype.add({
+	extension = {
+		templ = "templ",
+	},
+})
+
+vim.opt_global.conceallevel = 1
