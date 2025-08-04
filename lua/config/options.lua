2a843794908493348e3a2a3792e61b6f0673d516
roazzolini <guigoazzolini@gmail.com>
2025-08-03 23:47:31 -0300

    dunno dont remember

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,6 +23,7 @@ vim.o.inccommand = "split"
 vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.confirm = true
+
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
